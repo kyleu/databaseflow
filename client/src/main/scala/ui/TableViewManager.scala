3f2b9b46dd2e40865962937c65bd8fd685d233f8
Kyle Unverferth <kyle@kyleu.com>
2016-03-26 01:37:19 -0600

    Table view improvements, new icons.

--- client/src/main/scala/ui/TableViewManager.scala
+++ client/src/main/scala/ui/TableViewManager.scala
@@ -10,25 +10,26 @@ import org.scalajs.jquery.{ JQueryEventObject, jQuery => $ }
 object TableViewManager {
   var openTables = Map.empty[String, UUID]
 
-  def viewTable(table: Table, sendMessage: (RequestMessage) => Unit) = {
-    val queryId = UUID.randomUUID
+  def viewTable(table: Table, sendMessage: (RequestMessage) => Unit) = openTables.get(table.name) match {
+    case Some(queryId) =>
+      TabManager.selectTab(queryId)
+    case None =>
+      val queryId = UUID.randomUUID
+      TabManager.initIfNeeded()
+      WorkspaceManager.append(TableViewTemplate.forTable(queryId, table).toString)
 
-    TabManager.initIfNeeded()
+      TabManager.addTab(queryId, table.name, "folder-open-o")
 
-    WorkspaceManager.append(TableViewTemplate.forTable(queryId, table).toString)
+      val queryPanel = $(s"#panel-$queryId")
 
-    TabManager.addTab(queryId, table.name, "map-o")
+      QueryManager.activeQueries = QueryManager.activeQueries :+ queryId
 
-    val queryPanel = $(s"#panel-$queryId")
+      $(".fa-close", queryPanel).click({ (e: JQueryEventObject) =>
+        openTables = openTables - table.name
+        QueryManager.closeQuery(queryId, None, sendMessage)
+        false
+      })
 
-    QueryManager.activeQueries = QueryManager.activeQueries :+ queryId
-
-    $(".fa-close", queryPanel).click({ (e: JQueryEventObject) =>
-      openTables = openTables - table.name
-      QueryManager.closeQuery(queryId, None, sendMessage)
-      false
-    })
-
-    openTables = openTables + (table.name -> queryId)
+      openTables = openTables + (table.name -> queryId)
   }
 }
