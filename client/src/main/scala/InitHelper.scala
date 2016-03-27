74f1c09f08a9be9c7805e2ead76334c29123b63d
Kyle Unverferth <kyle@kyleu.com>
2016-03-27 02:05:53 -0600

    Autocomplete for the editor.

--- client/src/main/scala/InitHelper.scala
+++ client/src/main/scala/InitHelper.scala
@@ -1,13 +1,11 @@
 import org.scalajs.jquery.{ JQueryEventObject, jQuery => $ }
-import ui.{ AdHocQueryManager, SearchManager }
+import ui.{ AdHocQueryManager, EditorManager, SearchManager }
 import utils.Logging
 
 import scala.scalajs.js
 
 trait InitHelper { this: DatabaseFlow =>
   protected[this] def init() {
-    js.Dynamic.global.ace.require("ace/ext/language_tools")
-
     $("#new-query-link").click({ (e: JQueryEventObject) =>
       AdHocQueryManager.addNewQuery(sendMessage)
       false
@@ -16,6 +14,7 @@ trait InitHelper { this: DatabaseFlow =>
     js.Dynamic.global.$(".button-collapse").sideNav()
     js.Dynamic.global.$("select").material_select()
 
+    EditorManager.initEditorFramework()
     SearchManager.init()
 
     Logging.info("Database Flow Started.")
--- client/src/main/scala/ui/EditorManager.scala
+++ client/src/main/scala/ui/EditorManager.scala
@@ -5,12 +5,37 @@ import java.util.UUID
 import scala.scalajs.js
 
 object EditorManager {
+  private[this] def convertToJs(keys: Seq[String], meta: String) = keys.map { key =>
+    js.Dynamic.literal(
+      caption = key,
+      value = key,
+      meta = meta
+    )
+  }
+
+  private[this] def completionsFor(editor: js.Any, session: js.Any, pos: js.Any, prefix: String, callback: js.Dynamic) {
+    MetadataManager.schema.map { sch =>
+      val matches = convertToJs(sch.tables, "table") ++ convertToJs(sch.views, "view") ++ convertToJs(sch.procedures, "procedure")
+      callback(null, js.Array(matches: _*))
+    }
+  }
+
+  def initEditorFramework() = {
+    val langTools = js.Dynamic.global.ace.require("ace/ext/language_tools")
+
+    val completer = js.Dynamic.literal(
+      getCompletions = completionsFor _
+    )
+
+    langTools.addCompleter(completer)
+  }
+
   def initSqlEditor(id: UUID) = {
     val editor = js.Dynamic.global.ace.edit(s"sql-textarea-$id")
     //editor.setTheme("ace/theme/monokai")
     editor.setShowPrintMargin(false)
     editor.setHighlightActiveLine(false)
-    //editor.setAutoScrollEditorIntoView(true)
+    editor.setAutoScrollEditorIntoView(false)
     editor.getSession().setMode("ace/mode/sql")
     editor.getSession().setTabSize(2)
     editor.setOptions(js.Dynamic.literal(
