6c33f021ad1b0fb2e2d865aa25b14a55010e17a0
Kyle Unverferth <kyle@kyleu.com>
2016-03-26 23:59:17 -0600

    Lots of dependency upgrades.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   }
 
   object Logging {
-    val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.18"
+    val slf4jApi = "org.slf4j" % "slf4j-api" % "1.7.19"
   }
 
   object Hibernate {
@@ -14,7 +14,7 @@ object Dependencies {
   }
 
   object Jdbc {
-    val hikariCp = "com.zaxxer" % "HikariCP" % "2.4.3" // 2.4.4 has a weird sbt bug
+    val hikariCp = "com.zaxxer" % "HikariCP" % "2.4.5"
     val h2 = "com.h2database" % "h2" % "1.4.191"
     val mysql = "mysql" % "mysql-connector-java" % "5.1.38"
     val postgres = "org.postgresql" % "postgresql" % "9.4.1208"
@@ -28,7 +28,7 @@ object Dependencies {
   }
 
   object Serialization {
-    val version = "0.3.8"
+    val version = "0.3.9"
     val uPickle = "com.lihaoyi" %% "upickle" % version
   }
 
@@ -53,14 +53,14 @@ object Dependencies {
   }
 
   object WebJars {
-    val requireJs = "org.webjars" % "requirejs" % "2.1.22"
-    val jquery = "org.webjars" % "jquery" % "2.2.1"
+    val requireJs = "org.webjars" % "requirejs" % "2.2.0"
+    val jquery = "org.webjars" % "jquery" % "2.2.2"
     val materialize = "org.webjars" % "materializecss" % "0.97.5"
     val fontAwesome = "org.webjars" % "font-awesome" % "4.5.0"
   }
 
   object Mail {
-    val mailer = "com.typesafe.play" %% "play-mailer" % "3.0.1"
+    val mailer = "com.typesafe.play" %% "play-mailer" % "4.0.0"
   }
 
   object Ui {
@@ -68,14 +68,14 @@ object Dependencies {
   }
 
   object Metrics {
-    val metrics = "nl.grons" %% "metrics-scala" % "3.5.2"
+    val metrics = "nl.grons" %% "metrics-scala" % "3.5.3"
     val jvm = "io.dropwizard.metrics" % "metrics-jvm" % "3.1.2"
     val ehcache = "io.dropwizard.metrics" % "metrics-ehcache" % "3.1.2" intransitive()
     val healthChecks = "io.dropwizard.metrics" % "metrics-healthchecks" % "3.1.2" intransitive()
 
     val json = "io.dropwizard.metrics" % "metrics-json" % "3.1.2"
 
-    val jettyServlet = "org.eclipse.jetty" % "jetty-servlet" % "9.3.7.v20160115"
+    val jettyServlet = "org.eclipse.jetty" % "jetty-servlet" % "9.3.8.v20160314"
     val servlets = "io.dropwizard.metrics" % "metrics-servlets" % "3.1.2" intransitive()
     val graphite = "io.dropwizard.metrics" % "metrics-graphite" % "3.1.2" intransitive()
   }
