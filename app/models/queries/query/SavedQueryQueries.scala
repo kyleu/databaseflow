10faa8ab81a6bf14cba7a6c8f3f26c6336c5b17e
Kyle Unverferth <kyle@kyleu.com>
2016-03-30 00:30:30 -0600

    Start of query saving.

--- app/models/queries/query/SavedQueryQueries.scala
+++ app/models/queries/query/SavedQueryQueries.scala
@@ -9,8 +9,8 @@ import utils.DateUtils
 
 object SavedQueryQueries extends BaseQueries[SavedQuery] {
   override protected val tableName = "saved_queries"
-  override protected val columns = Seq("id", "owner", "title", "sql", "last_ran", "created", "updated")
-  override protected val searchColumns = Seq("id", "owner", "title", "sql")
+  override protected val columns = Seq("id", "title", "sql", "owner", "connection", "public", "last_ran", "created", "updated")
+  override protected val searchColumns = Seq("id", "owner", "connection", "title", "sql")
 
   val insert = Insert
   def getById(id: UUID) = GetById(Seq(id))
@@ -30,9 +30,14 @@ object SavedQueryQueries extends BaseQueries[SavedQuery] {
   override protected def fromRow(row: Row) = {
     SavedQuery(
       id = row.as[UUID]("id"),
-      owner = row.asOpt[UUID]("owner"),
+
       title = row.as[String]("title"),
       sql = row.as[String]("sql"),
+
+      owner = row.asOpt[UUID]("owner"),
+      connection = row.asOpt[UUID]("connection"),
+      public = row.as[Boolean]("public"),
+
       lastRan = row.asOpt[java.sql.Timestamp]("last_ran").map(_.getTime),
       created = row.as[java.sql.Timestamp]("created").getTime,
       updated = row.as[java.sql.Timestamp]("updated").getTime
@@ -40,6 +45,6 @@ object SavedQueryQueries extends BaseQueries[SavedQuery] {
   }
 
   override protected def toDataSeq(q: SavedQuery) = {
-    Seq[Any](q.id, q.owner, q.title, q.sql, q.lastRan, new java.sql.Timestamp(q.created), new java.sql.Timestamp(q.updated))
+    Seq[Any](q.id, q.title, q.sql, q.owner, q.connection, q.public, q.lastRan, new java.sql.Timestamp(q.created), new java.sql.Timestamp(q.updated))
   }
 }
