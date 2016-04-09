02136ff52ab68038e33741f53a71e93ef6ccfcda
Kyle Unverferth <kyle@kyleu.com>
2016-04-09 03:33:47 -0600

    Search context matches.

--- client/src/main/scala/ui/SearchFilterManager.scala
+++ client/src/main/scala/ui/SearchFilterManager.scala
@@ -2,24 +2,26 @@ package ui
 
 import java.util.UUID
 
-import org.scalajs.jquery.JQuery
+import org.scalajs.jquery.{ JQuery, jQuery => $ }
 
 object SearchFilterManager {
+  private[this] lazy val mainMenu = $("#main-menu")
+
   private[this] def savedQueryFields(id: String) = SavedQueryManager.savedQueries.get(UUID.fromString(id)) match {
-    case Some(sq) => Seq(id, sq.name, sq.description.getOrElse(""), sq.sql)
-    case None => Seq(id)
+    case Some(sq) => Seq("id" -> id, "name" -> sq.name, "description" -> sq.description.getOrElse(""), "sql" -> sq.sql)
+    case None => Seq("id" -> id)
   }
   private[this] def tableFields(name: String) = MetadataManager.schema.flatMap(_.tables.find(_.name == name)) match {
-    case Some(t) => Seq(name, t.description.getOrElse("")) ++ t.columns.map(_.name)
-    case None => Seq(name)
+    case Some(t) => Seq("name" -> name, "description" -> t.description.getOrElse("")) ++ t.columns.map(c => "name" -> c.name)
+    case None => Seq("name" -> name)
   }
   private[this] def viewFields(name: String) = MetadataManager.schema.flatMap(_.views.find(_.name == name)) match {
-    case Some(v) => Seq(name, v.description.getOrElse("")) ++ v.columns.map(_.name)
-    case None => Seq(name)
+    case Some(v) => Seq("name" -> name, "description" -> v.description.getOrElse("")) ++ v.columns.map(c => "column" -> c.name)
+    case None => Seq("name" -> name)
   }
   private[this] def procedureFields(name: String) = MetadataManager.schema.flatMap(_.procedures.find(_.name == name)) match {
-    case Some(p) => Seq(name, p.description.getOrElse("")) ++ p.params.map(_.name)
-    case None => Seq(name)
+    case Some(p) => Seq("name" -> name, "description" -> p.description.getOrElse("")) ++ p.params.map(p => "parameter" -> p.name)
+    case None => Seq("name" -> name)
   }
 
   def filterSchema(searches: Seq[String]) = {
@@ -37,7 +39,7 @@ object SearchFilterManager {
     }
   }
 
-  private[this] def highlightMatches(title: String, matches: Seq[String], j: JQuery) = {
+  private[this] def highlightTitleMatches(title: String, matches: Seq[String], j: JQuery) = {
     val replaced = matches.foldLeft(title) { (x, y) =>
       val titleLc = title.toLowerCase
       val idx = titleLc.indexOf(y)
@@ -47,19 +49,46 @@ object SearchFilterManager {
     j.html(html)
   }
 
-  private[this] def filterObjects(key: String, seq: Seq[(String, JQuery, JQuery)], searches: Seq[String], toggle: JQuery, searchF: (String) => Seq[String]) = {
-    val (matched, notMatched) = seq.partition(t => searchF(t._1).exists(matchName(searches, _)))
-    matched.foreach { o =>
-      highlightMatches(o._3.attr("title").getOrElse(o._1), searches, o._3)
-      o._2.show()
+  private[this] def highlightAdditionalMatches(title: String, keys: Seq[(String, String)], matches: Seq[String], j: JQuery) = {
+    val els = keys.map { key =>
+      val replaced = matches.foldLeft(key._2) { (x, y) =>
+        val keyLc = key._2.toLowerCase
+        val idx = keyLc.indexOf(y)
+        if (idx == -1) { key._2 } else { s"""${key._2.substring(0, idx)}[${key._2.substring(idx, idx + y.length)}]${key._2.substring(idx + y.length)}""" }
+      }
+      s"""Matches ${key._1} "$replaced"."""
     }
-    notMatched.foreach(_._2.hide())
+    j.attr("title", els.mkString("\n"))
+  }
+
+  private[this] def filterObjects(
+    key: String, seq: Seq[(String, JQuery, JQuery)], searches: Seq[String], toggle: JQuery, searchF: (String) => Seq[(String, String)]
+  ) = {
+    val matches = seq.map { t =>
+      val values = searchF(t._1)
+      val results = values.flatMap(v => matchKey(searches, v))
+      t -> results
+    }
+
+    val (matched, notMatched) = matches.partition(_._2.nonEmpty)
+    matched.foreach { o =>
+      o._2.find(_._1 == "name") match {
+        case Some(name) => highlightTitleMatches(name._2, searches, o._1._3)
+        case None => highlightAdditionalMatches(o._1._3.attr("title").getOrElse(o._1._1), o._2, searches, o._1._3)
+      }
+      o._1._2.show()
+    }
+    notMatched.foreach(_._1._2.hide())
     if (matched.isEmpty) { SearchManager.closeIfOpen(toggle) } else { SearchManager.openIfClosed(toggle) }
     //utils.Logging.info(s"Matched [${matched.size}] and skipped [${notMatched.size}] ${key}s.")
   }
 
-  private[this] def matchName(searches: Seq[String], name: String) = {
-    val lcn = name.toLowerCase
-    searches.forall(s => lcn.contains(s))
+  private[this] def matchKey(searches: Seq[String], key: (String, String)) = {
+    val lcn = key._2.toLowerCase
+    if (searches.forall(s => lcn.contains(s))) {
+      Some(key)
+    } else {
+      None
+    }
   }
 }
