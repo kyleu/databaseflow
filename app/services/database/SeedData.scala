ec37285403e89d057fd27397f672cc5159ca683d
Kyle Unverferth <kyle@kyleu.com>
2016-04-10 16:19:34 -0600

    Seed data cleanup.

--- app/services/database/SeedData.scala
+++ app/services/database/SeedData.scala
@@ -3,71 +3,63 @@ package services.database
 import java.util.UUID
 
 import models.engine.ConnectionSettings
-import models.engine.rdbms.{ H2, MySQL, PostgreSQL }
+import models.engine.rdbms._
 import models.query.SavedQuery
 import services.query.SavedQueryService
 
 object SeedData {
   def insert(db: DatabaseConnection) = {
+    addConnections()
+    addSavedQueries()
+  }
 
-    // ConnectionSettings
+  private[this] def addConnections() = {
     ConnectionSettingsService.insert(ConnectionSettings(
-      id = UUID.randomUUID,
-      name = "PostgreSQL Sample",
+      id = UUID.randomUUID, name = "PostgreSQL Sample",
       description = "The pagila sample database provided by the community.",
-      engine = PostgreSQL,
-      url = "jdbc:postgresql://localhost:5432/pagila",
-      username = "databaseflow",
-      password = "flow"
+      engine = PostgreSQL, url = "jdbc:postgresql://localhost:5432/pagila",
+      username = "databaseflow", password = "flow"
     ))
 
     ConnectionSettingsService.insert(ConnectionSettings(
-      id = UUID.randomUUID,
-      name = "MySQL Sample",
+      id = UUID.randomUUID, name = "MySQL Sample",
       description = "The salika sample database provided by the community.",
-      engine = MySQL,
-      url = "jdbc:mysql://localhost/sakila",
-      username = "root",
-      password = ""
+      engine = MySQL, url = "jdbc:mysql://localhost/sakila",
+      username = "root", password = ""
     ))
 
     ConnectionSettingsService.insert(ConnectionSettings(
-      id = UUID.randomUUID,
-      name = "Local H2",
+      id = UUID.randomUUID, name = "Oracle Test",
+      description = "The test database for my Oracle VM.",
+      engine = Oracle, url = "jdbc:oracle:thin:@//10.211.55.5:1521/XE",
+      username = "databaseflow", password = "flow"
+    ))
+
+    ConnectionSettingsService.insert(ConnectionSettings(
+      id = UUID.randomUUID, name = "SQL Server Test",
+      description = "The test database for my SQL Server VM.",
+      engine = SqlServer, url = "jdbc:sqlserver://10.211.55.5:1433;databaseName=databaseflow",
+      username = "databaseflow", password = "flow"
+    ))
+
+    ConnectionSettingsService.insert(ConnectionSettings(
+      id = UUID.randomUUID, name = "Local H2",
       description = "A scratchpad database to play around in.",
-      engine = H2,
-      url = "jdbc:h2:~/database.h2db",
-      username = "",
-      password = ""
+      engine = H2, url = "jdbc:h2:~/database.h2db",
+      username = "", password = ""
     ))
 
     ConnectionSettingsService.insert(ConnectionSettings(
-      id = UUID.randomUUID,
-      name = "AppThis Local",
+      id = UUID.randomUUID, name = "AppThis Local",
       description = "The local database for AppThis v2.",
-      engine = MySQL,
-      url = "jdbc:mysql://localhost/appthis_local",
-      username = "appthis",
-      password = "Mah14Mah1"
+      engine = MySQL, url = "jdbc:mysql://localhost/appthis_local",
+      username = "appthis", password = "Mah14Mah1"
     ))
+  }
 
-    // Saved Queries
-    SavedQueryService.save(SavedQuery(
-      name = "Saved Query 1",
-      sql = "select * from stuff",
-      public = true
-    ))
-
-    SavedQueryService.save(SavedQuery(
-      name = "Saved Query 2",
-      sql = "select * from stuff2",
-      public = true
-    ))
-
-    SavedQueryService.save(SavedQuery(
-      name = "Saved Query 3",
-      sql = "select * from stuff3",
-      public = true
-    ))
+  private[this] def addSavedQueries() = {
+    SavedQueryService.save(SavedQuery(name = "Saved Query 1", sql = "select * from stuff1", public = true))
+    SavedQueryService.save(SavedQuery(name = "Saved Query 2", sql = "select * from stuff2", public = true))
+    SavedQueryService.save(SavedQuery(name = "Saved Query 3", sql = "select * from stuff3", public = true))
   }
 }
--- shared/src/main/scala/models/engine/EngineQueries.scala
+++ shared/src/main/scala/models/engine/EngineQueries.scala
@@ -5,20 +5,20 @@ import models.engine.rdbms._
 object EngineQueries {
   def selectFrom(name: String, limit: Option[Int] = None, offset: Option[Int] = None)(implicit engine: DatabaseEngine) = {
     val preColumns = limit match {
-      case Some(l) if engine == SqlServer => "top 1 "
+      case Some(l) if engine == SqlServer => s"top $l "
       case _ => ""
     }
     val whereClauseAdditions = limit match {
-      case Some(l) if engine == Oracle => Some(" rownum <= " + l)
+      case Some(l) if engine == Oracle => Some(s" rownum <= $l")
       case _ => None
     }
     val postQueryClauses = limit match {
-      case Some(l) if engine == H2 || engine == MySQL || engine == PostgreSQL => " limit " + l
+      case Some(l) if engine == H2 || engine == MySQL || engine == PostgreSQL => s" limit $l"
       case _ => ""
     }
 
     val quotedName = engine.leftQuoteIdentifier + name + engine.rightQuoteIdentifier
-    val whereClause = whereClauseAdditions.map(" where " + _).getOrElse("")
+    val whereClause = whereClauseAdditions.map(" where" + _).getOrElse("")
     val sql = s"select $preColumns* from $quotedName$whereClause$postQueryClauses"
     sql
   }
