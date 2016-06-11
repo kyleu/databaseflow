d7897fc6736003f30350e8798a5b4b7442bdf3b8
Kyle Unverferth <kyle@kyleu.com>
2016-06-11 13:52:13 -0600

    Site fixes, better sidenav.

--- shared/src/main/scala/utils/TipsAndTricks.scala
+++ shared/src/main/scala/utils/TipsAndTricks.scala
@@ -7,6 +7,7 @@ sealed abstract class TipsAndTricks(val content: String) extends EnumEntry
 object TipsAndTricks extends Enum[TipsAndTricks] {
   case object NewQuery extends TipsAndTricks("Press \"+\" when outside of a sql editor to start a new query.")
   case object SwitchTabs extends TipsAndTricks("Press \"[\" or \"]\" when outside of a sql editor to switch between tabs.")
+  case object OpenInNewTab extends TipsAndTricks("Almost all the links in Database Flow can be opened in new tabs for easy side-by-side comparisons.")
 
   override val values = findValues
 }
