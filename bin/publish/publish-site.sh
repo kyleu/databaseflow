68f54d61d6cdb677bd90cf774bc5003eb52c553b
Kyle Unverferth <kyle@kyleu.com>
2016-07-04 17:11:35 -0600

    Publish script for site.

--- bin/publish/publish-site.sh
+++ bin/publish/publish-site.sh
@@ -10,3 +10,5 @@ rm -rf ./build/publish/databaseflow-site-1.0.0
 unzip ./site/target/universal/databaseflow-site-1.0.0.zip -d ./site/target/universal
 mv ./site/target/universal/databaseflow-site-1.0.0 ./build/publish/
 mv ./build/publish/databaseflow-site-1.0.0/ ./build/publish/databaseflow-site
+
+rsync -zrv --delete -e "ssh -i /Users/kyle/.ssh/aws-ec2-key.pem" ./build/publish/databaseflow-site/* ubuntu@databaseflow.com:~/deploy/databaseflow-site
