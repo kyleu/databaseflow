0d1ba2d598ca7d186ab8dbe32c96efcd02811c68
Kyle Unverferth <kyle@kyleu.com>
2016-07-04 20:53:19 -0600

    Key updates.

--- bin/config/letsencrypt/renewLetsEncrypt.sh
+++ bin/config/letsencrypt/renewLetsEncrypt.sh
@@ -1,2 +1,8 @@
-/opt/letsencrypt
-./letsencrypt-auto certonly -a webroot --webroot-path=/usr/share/nginx/html -d databaseflow.com -d www.databaseflow.com
+#!/bin/bash
+
+cd /opt/letsencrypt
+./letsencrypt-auto certonly -a webroot --webroot-path=/usr/share/nginx/html -d databaseflow.com -d www.databaseflow.com -d dbflow.com -d www.dbflow.com
+
+cd /home/ubuntu/letsencrypt
+sudo cp -LrR /etc/letsencrypt/live/databaseflow.com .
+sudo chown -R ubuntu:ubuntu databaseflow.com
