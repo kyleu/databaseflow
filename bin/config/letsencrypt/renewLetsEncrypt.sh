2e907cbb8bc7a3e492aaa04f5a2a423fd956787b
Kyle Unverferth <kyle@kyleu.com>
2016-07-04 22:39:25 -0600

    Key issuance scripts and config.

--- bin/config/letsencrypt/renewLetsEncrypt.sh
+++ bin/config/letsencrypt/renewLetsEncrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /opt/letsencrypt
-./letsencrypt-auto certonly -a webroot --webroot-path=/usr/share/nginx/html -d databaseflow.com -d www.databaseflow.com -d dbflow.com -d www.dbflow.com
+./letsencrypt-auto certonly -a webroot --webroot-path=/usr/share/nginx/html -d databaseflow.com -d www.databaseflow.com -d demo.databaseflow.com -d dbflow.com -d www.dbflow.com -d demo.dbflow.com
 
 cd /home/ubuntu/letsencrypt
 sudo cp -LrR /etc/letsencrypt/live/databaseflow.com .
