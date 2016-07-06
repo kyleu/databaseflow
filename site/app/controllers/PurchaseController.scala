ef303b4d028aed8805a3bd96d0a66a6c3b11d4a1
Kyle Unverferth <kyle@kyleu.com>
2016-07-06 00:07:07 -0600

    Stripe integration.

--- site/app/controllers/PurchaseController.scala
+++ site/app/controllers/PurchaseController.scala
@@ -1,9 +1,27 @@
 package controllers
 
+import licensing.{License, LicenseEdition, LicenseGenerator}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.data._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
+import utils.web.PlayFormUtils
 
 import scala.concurrent.Future
+
+object PurchaseController {
+  case class StripePurchaseForm(name: String, email: String, stripeToken: String, stripeTokenType: String)
+
+  val stripePurchaseForm = Form(
+    mapping(
+      "name" -> nonEmptyText,
+      "email" -> email,
+      "stripeToken" -> nonEmptyText,
+      "stripeTokenType" -> nonEmptyText
+    )(StripePurchaseForm.apply)(StripePurchaseForm.unapply)
+  )
+}
 
 @javax.inject.Singleton
 class PurchaseController @javax.inject.Inject() (implicit override val messagesApi: MessagesApi) extends Controller with I18nSupport {
@@ -12,6 +30,15 @@ class PurchaseController @javax.inject.Inject() (implicit override val messagesA
   }
 
   def purchaseTeamEdition() = Action.async { implicit request =>
-    Future.successful(Ok("OK"))
+    PurchaseController.stripePurchaseForm.bindFromRequest.fold(
+      formWithErrors => {
+        Future.successful(Redirect(controllers.routes.PurchaseController.pricing()).flashing("error" -> PlayFormUtils.errorsToString(formWithErrors.errors)))
+      },
+      f => {
+        val license = License(name = f.name, email = f.email, edition = LicenseEdition.Team)
+        LicenseGenerator.saveLicense(license)
+        Future.successful(Redirect(controllers.routes.UserLicenseController.licenseView(license.id)))
+      }
+    )
   }
 }
