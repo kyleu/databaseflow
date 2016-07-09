4e59d319a318c012f8d1d129c37a1ac4f7e9e1a6
Kyle Unverferth <kyle@kyleu.com>
2016-07-09 14:03:53 -0600

    Download routes, site fixes.

--- site/app/controllers/DownloadController.scala
+++ site/app/controllers/DownloadController.scala
@@ -2,16 +2,52 @@ package controllers
 
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
+import play.twirl.api.Html
+import utils.Logging
 
 import scala.concurrent.Future
 
 @javax.inject.Singleton
-class DownloadController @javax.inject.Inject() (implicit override val messagesApi: MessagesApi) extends Controller with I18nSupport {
-  def personalEdition() = Action.async { implicit request =>
-    Future.successful(Ok("Personal Edition!"))
+class DownloadController @javax.inject.Inject() (implicit override val messagesApi: MessagesApi) extends Controller with I18nSupport with Logging {
+  private[this] val downloadDir = {
+    val server = new java.io.File("/home/ubuntu/deploy/databaseflow-downloads")
+    if (server.exists && server.isDirectory) {
+      server
+    } else {
+      val local = new java.io.File("./build")
+      if (local.exists && local.isDirectory) {
+        log.info("Using local directory for downloads.")
+        local
+      } else {
+        throw new IllegalStateException("Cannot find download directory.")
+      }
+    }
   }
 
-  def teamEdition() = Action.async { implicit request =>
-    Future.successful(Ok("Team Edition!"))
+  def download(os: String, variant: String) = Action.async { implicit request =>
+    val filename = os match {
+      case "osx" => variant match {
+        case "service" => "osx/databaseflow.zip"
+        case "gui" => "osx/databaseflow.dmg"
+        case x => throw new IllegalStateException(s"Unknown variant [$variant].")
+      }
+      case "linux" => variant match {
+        case "service" => "linux/databaseflow.tgz"
+        case "deb" => "linux/databaseflow_1.0.0_all.deb"
+        case x => throw new IllegalStateException(s"Unknown variant [$variant].")
+      }
+      case "windows" => variant match {
+        case "service" => "databaseflow.zip"
+        case "gui" => "databaseflow.msi"
+        case x => throw new IllegalStateException(s"Unknown variant [$variant].")
+      }
+      case x => throw new IllegalStateException(s"Unknown os [$os].")
+    }
+    val file = new java.io.File(downloadDir, filename)
+    if (file.exists) {
+      Future.successful(Ok.sendFile(file))
+    } else {
+      Future.successful(NotFound(Html(s"<body>We're sorry, we couldn't find that download.<!-- ${file.getAbsolutePath} --></body>")))
+    }
   }
 }
