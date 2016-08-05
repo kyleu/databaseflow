993ad967cfb64169def69d403a6c27161b9182ee
Kyle Unverferth <kyle@kyleu.com>
2016-08-04 21:42:23 -0600

    System import/export.

--- app/controllers/admin/SystemExportController.scala
+++ app/controllers/admin/SystemExportController.scala
@@ -7,7 +7,9 @@ import services.connection.ConnectionSettingsService
 import services.query.SavedQueryService
 import services.settings.SettingsService
 import services.user.UserService
+import upickle.Js
 import utils.ApplicationContext
+import upickle.default._
 
 import scala.concurrent.Future
 
@@ -15,21 +17,18 @@ import scala.concurrent.Future
 class SystemExportController @javax.inject.Inject() (override val ctx: ApplicationContext, userService: UserService) extends BaseController {
   def systemExport = withAdminSession("admin-system-export") { implicit request =>
     val settings = SettingsService.getOverrides
-    val users = if (request.identity.role == Role.Admin) {
-      userService.getAll
-    } else {
-      Nil
-    }
     val connections = ConnectionSettingsService.getVisible(request.identity).map(c => c.copy(password = ""))
     val savedQueries = SavedQueryService.getVisible(request.identity.id)
-    val ret = ExportModel(settings, users, connections, savedQueries)
+    val ret = ExportModel(settings, connections, savedQueries)
 
-    val json = ret.toString
+    val json = writeJs(ret)
 
-    Future.successful(Ok(json))
+    Future.successful(Ok(json.toString).as("application/json"))
   }
 
   def systemImport = withAdminSession("admin-system-import") { implicit request =>
+    val json = request.body.asText.getOrElse(throw new IllegalArgumentException("Missing JSON body."))
+    val export = read[ExportModel](json)
     Future.successful(Ok("Imported!"))
   }
 }
--- app/models/settings/ExportModel.scala
+++ app/models/settings/ExportModel.scala
@@ -2,6 +2,5 @@ package models.settings
 
 import models.connection.ConnectionSettings
 import models.query.SavedQuery
-import models.user.User
 
-case class ExportModel(settings: Seq[Setting], users: Seq[User], connections: Seq[ConnectionSettings], savedQueries: Seq[SavedQuery])
+case class ExportModel(settings: Seq[Setting], connections: Seq[ConnectionSettings], savedQueries: Seq[SavedQuery])
