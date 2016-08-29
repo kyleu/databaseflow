f4525c1e368dffd8eb1923f55972a4f475895ee5
Kyle Unverferth <kyle@kyleu.com>
2016-08-28 23:07:17 -0600

    Minor permission changes.

--- client/src/main/scala/ui/modal/SharedResultFormManager.scala
+++ client/src/main/scala/ui/modal/SharedResultFormManager.scala
@@ -11,15 +11,15 @@ import utils.{NetworkMessage, TemplateUtils}
 import scala.scalajs.js
 
 object SharedResultFormManager {
-  val modal = js.Dynamic.global.$("#share-results-modal")
+  val modal = js.Dynamic.global.$("#shared-result-modal")
   private[this] var activeSharedResult: Option[SharedResult] = None
 
-  private[this] val inputTitle = $("#input-share-results-title", modal)
-  private[this] val inputDescription = $("#input-share-results-description", modal)
+  private[this] val inputTitle = $("#input-shared-result-title", modal)
+  private[this] val inputDescription = $("#input-shared-result-description", modal)
 
   def init() = {
-    TemplateUtils.clickHandler($("#input-share-results-cancel-link", modal), jq => modal.closeModal())
-    TemplateUtils.clickHandler($("#input-share-results-share-link", modal), jq => share())
+    TemplateUtils.clickHandler($("#input-shared-result-cancel-link", modal), jq => modal.closeModal())
+    TemplateUtils.clickHandler($("#input-shared-result-share-link", modal), jq => share())
   }
 
   def show(sharedResult: SharedResult) = {
@@ -28,10 +28,10 @@ object SharedResultFormManager {
     inputTitle.value(sharedResult.title)
     inputDescription.value(sharedResult.description.getOrElse(""))
     sharedResult.viewableBy match {
-      case Permission.Visitor => $("#input-share-results-visitor", modal).prop("checked", true)
-      case Permission.User => $("#input-share-results-user", modal).prop("checked", true)
-      case Permission.Administrator => $("#input-share-results-admin", modal).prop("checked", true)
-      case Permission.Private => $("#input-share-results-private", modal).prop("checked", true)
+      case Permission.Visitor => $("#input-shared-result-visitor", modal).prop("checked", true)
+      case Permission.User => $("#input-shared-result-user", modal).prop("checked", true)
+      case Permission.Administrator => $("#input-shared-result-admin", modal).prop("checked", true)
+      case Permission.Private => $("#input-shared-result-private", modal).prop("checked", true)
     }
 
     modal.openModal()
