bf4d724596f5793888c234609e9fedecdc819b9e
Kyle Unverferth <kyle@kyleu.com>
2016-09-17 05:28:56 -0600

    Site improvements.

--- bin/publish/publish-downloads.sh
+++ bin/publish/publish-downloads.sh
@@ -2,4 +2,4 @@
 
 cd ../../
 
-rsync -zrv -e "ssh -i /Users/kyle/.ssh/aws-ec2-key.pem" ./build/* ubuntu@databaseflow.com:~/deploy/databaseflow-downloads
+rsync -zrv -e "ssh -i /Users/kyle/.ssh/aws-ec2-key.pem" ./build/* ubuntu@databaseflow.com:~/deploy/databaseflow-downloads --exclude '/jwrapper'
--- util/jwrapper/src/main/scala/XmlHelper.scala
+++ util/jwrapper/src/main/scala/XmlHelper.scala
@@ -2,25 +2,24 @@ object XmlHelper {
   def xml(dir: String) = {
     val libFiles = new java.io.File(dir + "/target/universal/stage/lib").list()
     <jwrapper>
-      <BuildOutputFolder>{ dir }/tmp/jwrapper/build</BuildOutputFolder>
       <BundleName>Database Flow</BundleName>
       <SplashPNG>{ dir }/public/images/ui/favicon/favicon-install-splash.png</SplashPNG>
       <BundleLogoPNG>{ dir }/public/images/ui/favicon/favicon-512.png</BundleLogoPNG>
-      <InstallType>CurrentUser</InstallType>
+      <InstallType>NoInstall</InstallType>
       <App>
         <Name>Database Flow</Name>
         <LogoPNG>{ dir }/public/images/ui/favicon/favicon-512.png</LogoPNG>
         <MainClass>play.core.server.ProdServerStart</MainClass>
       </App>
       <SupportedLanguages>en</SupportedLanguages>
-      <Windows32JRE>{ dir }/tmp/jwrapper/jre-installs/win32/jre1.8.0_101</Windows32JRE>
+      <!-- Windows32JRE>{ dir }/tmp/jwrapper/jre-installs/win32/jre1.8.0_101</Windows32JRE -->
       <Windows64JRE>{ dir }/tmp/jwrapper/jre-installs/win64/jre1.8.0_101</Windows64JRE>
-      <Linux32JRE>{ dir }/tmp/jwrapper/jre-installs/linux/jre1.8.0_101</Linux32JRE>
-      <Linux64JRE>{ dir }/tmp/jwrapper/jre-installs/linuxx64/jre1.8.0_101</Linux64JRE>
-      <Mac64JRE>{ dir }/tmp/jwrapper/jre-installs/macos64/jre1.8.0_101.jre</Mac64JRE>
+      <!-- Linux32JRE>{ dir }/tmp/jwrapper/jre-installs/linux/jre1.8.0_101</Linux32JRE -->
+      <!-- Linux64JRE>{ dir }/tmp/jwrapper/jre-installs/linuxx64/jre1.8.0_101</Linux64JRE -->
+      <!-- Mac64JRE>{ dir }/tmp/jwrapper/jre-installs/macos64/jre1.8.0_101.jre</Mac64JRE -->
       <WindowsElevation>None</WindowsElevation>
       <WindowsElevationUiAccess>true</WindowsElevationUiAccess>
-      { for (f <- libFiles) yield <File classpath='yes'>{ dir }/target/universal/stage/lib/{ f }</File> }
+      { for (f <- libFiles) yield <File classpath='yes'>{ dir + "/target/universal/stage/lib/" + f }</File> }
       <JvmOptions>
         <JvmOption>-Xmx1024m</JvmOption>
         <JvmOption>-Dshow.gui=true</JvmOption>
