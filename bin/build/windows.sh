747d8676ca704f56b320d1556eea1513878329ad
Kyle Unverferth <kyle@kyleu.com>
2016-10-06 21:32:59 -0600

    Launch4j adjustments.

--- bin/build/windows.sh
+++ bin/build/windows.sh
@@ -6,7 +6,7 @@ sbt-nodebug assembly
 
 mv "./target/scala-2.11/databaseflow-assembly-1.0.0.jar" "./build/DatabaseFlow.jar"
 
-build/launch4j/launch4j/launch4j ~/Projects/Personal/databaseflow/bin/build/launch4j.osx.xml
+build/launch4j/launch4j/launch4j ~/Projects/Personal/databaseflow/bin/build/launch4j.xml
 
 cd build/launch4j
 zip -r "../DatabaseFlow.zip" . -x "launch4j/*"
