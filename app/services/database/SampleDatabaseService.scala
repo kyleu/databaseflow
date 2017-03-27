22f8c14aef657d79c51d12ab66ff6cb791195954
Kyle Unverferth <kyle@kyleu.com>
2017-03-26 21:33:09 -0600

    Saved queries for sample db.

--- app/services/database/SampleDatabaseService.scala
+++ app/services/database/SampleDatabaseService.scala
@@ -5,10 +5,13 @@ import java.util.UUID
 
 import models.connection.ConnectionSettings
 import models.engine.DatabaseEngine
+import models.queries.query.SavedQueryQueries
+import models.query.SavedQuery
 import models.user.Permission
 import org.apache.commons.io.IOUtils
 import services.config.ConfigFileService
 import services.connection.ConnectionSettingsService
+import services.database.core.MasterDatabase
 import utils.{Logging, SlugUtils}
 
 object SampleDatabaseService extends Logging {
@@ -19,20 +22,18 @@ object SampleDatabaseService extends Logging {
   def apply(owner: UUID) = {
     val sampleFile = getSampleFile
     val name = "SQLite Music Database"
-
     val cs = ConnectionSettings(
       id = UUID.randomUUID,
       name = name,
       slug = SlugUtils.slugFor(name),
       owner = owner,
-      edit = Permission.User,
+      edit = Permission.Administrator,
       description = "A sample SQLite database containing music information.",
       engine = DatabaseEngine.SQLite,
       dbName = Some(sampleFile.getPath)
     )
-
     ConnectionSettingsService.insert(cs)
-
+    createSavedQueries(owner, cs.id)
     cs
   }
 
@@ -49,5 +50,18 @@ object SampleDatabaseService extends Logging {
     val os = new FileOutputStream(f)
     IOUtils.copy(is, os)
     os.close()
+  }
+
+  private[this] def createSavedQueries(owner: UUID, connId: UUID) = {
+    def q(name: String, sql: String) = SavedQuery(name = name, sql = sql, owner = owner, connection = Some(connId))
+
+    val queries = Seq(
+      q("Test Query", "select * from table"),
+      q("Test Query 2", "select * from table2")
+    )
+
+    queries.foreach { q =>
+      MasterDatabase.executeUpdate(SavedQueryQueries.insert(q))
+    }
   }
 }
